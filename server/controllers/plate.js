075d3ff96490fbb3301001a258410c34cc5e62c2
Jiayii03 <116914556+Jiayii03@users.noreply.github.com>
2024-04-09 15:08:33 +0100

    fix: Added try catch, removed unlink()

--- server/controllers/plate.js
+++ server/controllers/plate.js
@@ -5,53 +5,57 @@ const FormData = require("form-data");
 const detectPlate = (req, res) => {
   const { filename } = req;
 
-  return new Promise((resolve, reject) => {
     const filePath = `uploads/${filename}`;
 
-    // Read the file from the file system
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        reject(err);
-        return;
-      }
 
-      // Convert the file content to base64
-      const base64String = data.toString("base64");
+    try {
+      // Read the file from the file system
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+  
+        // Convert the file content to base64
+        const base64String = data.toString("base64");
+  
+        // Create FormData
+        let body = new FormData();
+        body.append("upload", base64String);
+        body.append("regions", "us-ca"); // Change to your country
+  
+        // Send the base64 data to the API
+        axios
+          .post("https://api.platerecognizer.com/v1/plate-reader/", body, {
+            headers: {
+              ...body.getHeaders(),
+              Authorization: "Token " + process.env.PLATE_RECOGNIZER_TOKEN,
+            },
+          })
+          .then((response) => {
+            console.log(response.data);
+            res.send(response.data);
+            resolve(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+            res.send(error.response.data)
+            reject(error);
+          })
+          // .finally(() => {
+          //   // Delete the file from the file system
+          //   fs.unlink(filePath, (err) => {
+          //     if (err) {
+          //       console.error("Error deleting file:", err);
+          //       return;
+          //     }
+          //   });
+          // });
+      });
+    } catch (error) {
+      return res.status(400).send("Error: No file found");
+    }
 
-      // Create FormData
-      let body = new FormData();
-      body.append("upload", base64String);
-      body.append("regions", "us-ca"); // Change to your country
-
-      // Send the base64 data to the API
-      axios
-        .post("https://api.platerecognizer.com/v1/plate-reader/", body, {
-          headers: {
-            ...body.getHeaders(),
-            Authorization: "Token " + process.env.PLATE_RECOGNIZER_TOKEN,
-          },
-        })
-        .then((response) => {
-          console.log(response.data);
-          res.send(response.data);
-          resolve(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-          res.send(error.response.data)
-          reject(error);
-        })
-        .finally(() => {
-          // Delete the file from the file system
-          fs.unlink(filePath, (err) => {
-            if (err) {
-              console.error("Error deleting file:", err);
-              return;
-            }
-          });
-        });
-    });
-  });
 };
 
 const apiTest = (req, res) => {
