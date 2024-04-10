f9fa1ccc31eac1eb0b8616fe63fd5a26ab8e20eb
Jiayii03 <116914556+Jiayii03@users.noreply.github.com>
2024-04-10 01:41:02 +0100

    Updated Dockerfile, changed host to 0.0.0.0

--- patternDetection/app.py
+++ patternDetection/app.py
@@ -125,4 +125,4 @@ def detect_pattern():
         return jsonify({'message': 'No pattern detected', 'mean_kl_divergence': mean_kl_divergence, 'edge_density': edge_density})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
